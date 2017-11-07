0645a0348db3e0399caa00e6f216d2be59fa2d67
TheaZh <qz2271@columbia.edu>
2017-11-07 00:55:29 -0500

    course schema

---
+++ Course.py
@@ -0,0 +1,64 @@
+from pymongo import MongoClient
+
+class Course:
+    def __init__(self):
+        self.mongo_uri = "mongodb://assignment:assignment123@ds161873.mlab.com:61873/assignment-management-system"
+        self.client = MongoClient(self.mongo_uri)
+        self.db = self.client['assignment-management-system']
+        self.course_collection = self.db['Course']
+
+    def insert_course(self, course_id, course_name, assignment_list, grader_id):
+        seed_data = {'course_id' : course_id,
+                    'course_name': course_name,
+                    'assignment_list': assignment_list,
+                    'grader_id': grader_id
+                    }
+        self.course_collection.insert(seed_data)
+
+    def insert_many_courses(self, seed_data):
+        self.course_collection.insert_many(seed_data)
+
+    def close_gridFS(self):
+        self.client.close()
+
+if __name__ == '__main__':
+    '''course_data = [
+        {'course_id' : 'COMS6111',
+        'course_name': 'Advanced Database Systems',
+        'assignment_list': ['COMS6111_A1', 'COMS6111_A2'],
+        'grader_id': 'ac1007'
+        },
+            {'course_id' : 'ELEN6770',
+        'course_name': 'Next Generation Networking',
+        'assignment_list': ['ELEN6770_A1', 'ELEN6770_A2', 'ELEN6770_A3'],
+        'grader_id': 'ac1007'
+        },
+        {'course_id' : 'COMS4111',
+        'course_name': 'Introduction to Database Systems',
+        'assignment_list': ['COMS4111_A1', 'COMS4111_A2', 'COMS4111_A3'],
+        'grader_id': 'ac1007'
+        },
+        {'course_id' : 'COMS1234',
+        'course_name': 'Software Development',
+        'assignment_list': ['COMS1234_A1'],
+        'grader_id': 'xz1332'
+        },
+        {'course_id' : 'ELEN5567',
+        'course_name': 'Content Distribution Networks',
+        'assignment_list': ['ELEN5567_A1'],
+        'grader_id': 'lu5200'
+        },
+        {'course_id' : 'ELEN4377',
+        'course_name': 'Introduction to Computer Networks',
+        'assignment_list': ['ELEN4377_A1'],
+        'grader_id':'lu5200'
+        },
+        {'course_id' : 'COMS4396',
+        'course_name': 'Introduction to System Design',
+        'assignment_list': ['COMS4396_A1'],
+        'grader_id': 'xz1332'
+        }
+    ]
+    course = Course()
+    course.insert_many_courses(course_data)
+    course.close_gridFS()'''
