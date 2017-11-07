9179be2ee9de415de9fc90660babe2d69f18c03c
TheaZh <qz2271@columbia.edu>
2017-11-07 02:10:22 -0500

    gitignore

--- src/server.py
+++ src/server.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from pymongo import MongoClient
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def log_in():
+    return render_template('/pages/login.html')
+
 
 
 if __name__ == '__main__':
