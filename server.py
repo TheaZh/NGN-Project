bbb2e513cd25df12c14c4d8a3b3f6ab8c9a5598e
TheaZh <qz2271@columbia.edu>
2017-11-11 00:09:29 -0500

    minor

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for,jsonify
-import json
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+import json, os
 from pymongo import MongoClient
+from werkzeug.utils import secure_filename
+
 
 # normal MongoDB
 MONGO_URI = "mongodb://assignment:assignment123@ds161873.mlab.com:61873/assignment-management-system"
@@ -15,6 +17,9 @@ ASSIGNMENT = db['Assignment']
 # GF = MongoClient(GRIDFS_URI)
 # FILE_DB = GF['files-management']
 
+UPLOAD_FOLDER = '/Users/QW_Z/Desktop/NGN-Project/tmpFiles/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 
 app = Flask(__name__)
 
@@ -26,7 +31,7 @@ def root():
 
 @app.route('/login')
 def login():
-    user_pass = request.args # contains user's uni and password
+    user_pass = request.args  # contains user's uni and password
 
     uni = user_pass.get('uni')
     password = user_pass.get('password')
@@ -50,7 +55,7 @@ def login():
 def student(userUNI):
     user_uni = userUNI
 
-    user_data = USER.find_one({'uni':user_uni})
+    user_data = USER.find_one({'uni': user_uni})
     user_data.pop('_id')
     # print 'user_data ---', user_data
 
@@ -58,7 +63,7 @@ def student(userUNI):
     courses_data = []
     for one in courses_list:
         # one == course_id
-        tmp_course_data = COURSE.find_one({'course_id':one})
+        tmp_course_data = COURSE.find_one({'course_id': one})
         tmp_course_data.pop('_id')
         courses_data.append(tmp_course_data)
     # print 'all course -- ', courses_data
@@ -85,7 +90,7 @@ def student(userUNI):
 def show(userUNI):
     user_uni = json.loads(userUNI)['uni']
     # print user_uni
-    return render_template('student-page.html', user_uni = user_uni)
+    return render_template('student-page.html', user_uni=user_uni)
 
 
 @app.route('/get_grade')
@@ -97,7 +102,7 @@ def get_grade():
     # print 'course ID: ', course_id
     # print 'UNI: ', uni
 
-    course_info = COURSE.find_one({'course_id':course_id})
+    course_info = COURSE.find_one({'course_id': course_id})
     course_info.pop('_id')
 
     grader_uni = course_info['grader_id']
@@ -116,7 +121,7 @@ def get_grade():
             grade[assign_id] = grade_dict[uni]
     # print grade
     data = {
-        'grade' : grade,
+        'grade': grade,
         'course_info': course_info,
         'grader': grader_name
     }
@@ -127,20 +132,55 @@ def get_grade():
 @app.route('/get_assignment')
 def get_assignment():
     para = request.args
-    print 'get_assignment parameters: ', para
+    print('get_assignment parameters: ', para)
     # course_id = para.get('course_id')
     assignment_id = para.get('assignment_id')
     assignment_info = ASSIGNMENT.find_one({'assignment_id': assignment_id})
     assignment_info.pop('_id')
-    print assignment_info
+    print(assignment_info)
     return jsonify(assignment_info)
+
 
 @app.route('/get_uploaded_files')
 def get_uploaded_files():
+    # NOT finished Yet
     file_ids_list = request.args.get('file_ids_list')
+
+
+def allowed_file(filename):
+    # constraint the allowed extensions of uploading files
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/choose_files', methods=['GET', 'POST'])
+def choose_files():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            print('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            print('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            return redirect(url_for('save_files',
+                                    filename=filename))
+
+
+@app.route('/upload_files')
+def upload_files():
+    return False
+
 
 
 
 if __name__ == '__main__':
     app.debug = True
     app.run()
+    # upload_files()
